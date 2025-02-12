cf116e6b1faa8515f7eec76f01b8198bc1a95b87
Nifdi Guliyev <87927314+Nifdi01@users.noreply.github.com>
2025-02-12 17:31:36 +0400

    Implement main function to read puzzle and solve using NPuzzle class

--- main.py
+++ main.py
@@ -1,14 +1,29 @@
-from utils.read_puzzle import read_puzzle
+from n_puzzle import NPuzzle
+from utils import read_puzzle
+import sys
 
 def main():
     file_path = 'n-puzzle.txt'
     try:
         initial_state = read_puzzle(file_path)
-        print(initial_state)
     except Exception as e:
         print(f"Error reading puzzle file: {e}")
         return
     
+    heuristic = sys.argv[1]
+
+    try:
+        puzzle = NPuzzle(initial_state, heuristic=heuristic)
+    except ValueError as ve:
+        print(f"Invalid puzzle configuration: {ve}")
+        return
+
+    solution = puzzle.solve()
+    if solution!=-1:
+        print("Solution found in", solution, "moves:")
+    else:
+        print("No solution found.")
+        
     
 if __name__ == '__main__':
     main()

31489efcc800b1c242420c57b7ef76669a3e8229
Nifdi Guliyev <87927314+Nifdi01@users.noreply.github.com>
2025-02-12 17:31:06 +0400

    Implement NPuzzle class with heuristic-based solving and neighbor generation

---
+++ n_puzzle.py
@@ -0,0 +1,59 @@
+from queue import PriorityQueue
+from heuristics import *
+from utils import generate_goal_state, find_blank
+
+class NPuzzle:
+    def __init__(self, initial_state, heuristic=None):
+        self.n = len(initial_state)
+        for idx, row in enumerate(initial_state):
+            if len(row) != self.n:
+                raise ValueError(
+                    f"Row {idx+1} has {len(row)} elements but each row must have {self.n} elements with empty tab representing the blank."
+                )
+        self.initial_state = tuple(tuple(row) for row in initial_state)
+        if heuristic == "e":
+            print("found")
+            self.heuristic = euclidean
+        else:
+            self.heuristic = manhattan
+        self.goal_state = generate_goal_state(self.n)
+        
+    def get_neighbors(self, state):
+        blank_i, blank_j = find_blank(state, self.n)
+        neighbors = []
+        
+        moves = [(0, 1, 'Right'), (0, -1, 'Left'), (1, 0, 'Down'), (-1, 0, 'Up')]
+        
+        for di, dj, move in moves:
+            new_i, new_j = blank_i + di, blank_j + dj
+            if 0 <= new_i < self.n and 0 <= new_j < self.n:
+                new_state = [list(row) for row in state]
+                new_state[blank_i][blank_j], new_state[new_i][new_j] = new_state[new_i][new_j], new_state[blank_i][blank_j]
+                neighbors.append(tuple(tuple(row) for row in new_state))
+        return neighbors
+    
+    def solve(self):
+        priority_queue = PriorityQueue()
+        initial_heuristic = self.heuristic(self.n, self.initial_state)
+        
+        priority_queue.put((initial_heuristic, 0, self.initial_state))
+        visited = set()
+        
+        while not priority_queue.empty():
+            _, cost, current_state = priority_queue.get()
+            
+            if current_state == self.goal_state:
+                return cost
+            
+            if current_state in visited:
+                continue
+            
+            visited.add(current_state)
+            
+            for neighbor in self.get_neighbors(current_state):
+                if neighbor not in visited:
+                    new_cost = cost + 1
+                    h = self.heuristic(self.n, neighbor)
+                    priority_queue.put((new_cost + h, new_cost, neighbor))
+
+        return -1
